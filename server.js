9f073e1b1ee38bc26aaa89c7d4ed1f7ec34a7a7d
ivanAlbino <52820803+ivanAlbino@users.noreply.github.com>
2019-07-13 02:35:24 +0800

    Changes on Library + Server.js
    Added a functionality to get all the data from the items table in the Inventory database.

--- server.js
+++ server.js
@@ -2,16 +2,31 @@
  * Description: Loads the app server using express
  */
 
- const express = require('express');
- const app = express();
+ const express = require('express')
+ const app = express()
+ const mysql = require('mysql')
 
  function msg(msg){
-    console.log(msg);
+    console.log(msg)
 }
 
-app.get("/",(req,res) =>{
-    msg("Responding to root route")
-    res.send("Hello")
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'AB12',
+    database: 'inventory'
+})
+
+connection.connect()
+
+//Retrieve all items
+app.get("/items",(req,res) =>{
+
+    connection.query("SELECT * FROM items", (err,rows,fields)=>{
+        msg("Fetched users successfully")
+        res.json(rows)
+    })
+
 })
 
  app.listen(3000,() =>{
